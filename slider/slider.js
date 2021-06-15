b54f3a5975e9b233c2c0c9fb67b76d4bce4fe70b
Kventura <81574011+keni-afk@users.noreply.github.com>
2021-06-15 01:53:38 -0500

    new.agg

--- slider/slider.js
+++ slider/slider.js
@@ -41,3 +41,42 @@ btnLeft.addEventListener('click' , function(){
 setInterval(function(){
     Next();
 }, 3000);
+
+const container = document.querySelectorAll(".container .card")
+
+        container.forEach(el => {
+            el.addEventListener("mousemove",(e)=> {
+                let xAxis = (window.innerWidth/2-e.pageX)/15
+                let yAxis = (window.innerHeight/2-e.pageY)/15
+                el.style.transform =`rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+            })
+        })
+        //in
+        container.forEach(el => {
+            el.addEventListener('mouseenter',(e)=>{
+                el.style.transition =`all 0.5s ease`
+                el.style.animation =`animacion 1.5s forwards`
+                el.querySelector(".title").style.transform ="translateZ(150px)"
+            })
+        })
+        //out
+        container.forEach(el => {
+            el.addEventListener('mouseleave',(e)=>{
+                el.style.transition =`all 1.5s ease`
+                el.style.animation =`none`
+                el.style.transform =`rotateY(0deg) rotateX(0deg)`
+                el.querySelector(".title").style.transform ="translateZ(0px)"
+            })
+        })
+
+        function musicaTends(){
+            var $musica= 10;
+            
+            if ($musica>5) {
+                            $musica= "«««Rauw Alejandro»»»   "+ "    ===>Todo de Ti<===";
+            }
+            else{
+                            $musica= "No tenemos tendencias nuevas de momento";
+            }
+        document.getElementById("convert").innerHTML =$musica;
+}     
